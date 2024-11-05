7439fad0ef22b41156bfbe65837b0a580d7a1318
micailamarcelle <micailamarcelle@arizona.edu>
2024-11-04 20:19:30 -0700

    Got the layout functioning properly, and began implementing actual functionality for addBooks

--- MyLibraryGUI.java
+++ MyLibraryGUI.java
@@ -10,15 +10,32 @@ functionalities provided by the text-based view, but in a different, slightly mo
 convenient format.
  */
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.*;
+import java.io.File;
+
 import javax.swing.*;
 
 public class MyLibraryGUI extends JFrame {
     private LibraryCollectionController controller;
     private JPanel panel;
-    private JTextField primaryTextField;
     private JLabel primaryLabel;
-    private JLabel buttonOutputText;
+    private JComboBox<String> optionsComboBox;
+    private JLabel buttonOutputLabel;
+    private JButton comboBoxSubmitButton;
+
+    private JFrame addBooksWindow;
+    private JTextField addBooksTextField;
+    private JLabel addBooksErrorLabel;
+
+    // Main method
+    public static void main(String[] args) {
+        MyLibraryGUI view = new MyLibraryGUI();
+        view.start();
+        view.setVisible(true);
+    }
 
     /*
         Constructor for the GUI
@@ -37,33 +54,37 @@ public class MyLibraryGUI extends JFrame {
 
         // Sets up the label which will be situated at the top of the window, explaining how
         // to use the GUI
-        String textAtTop = "---------------------------\n";
-        textAtTop += "- WELCOME TO YOUR LIBRARY -\n";
-        textAtTop += "---------------------------\n\n";
-        textAtTop += "Enter one of the following commands in the text box,\n";
-        textAtTop += "then press the 'Enter' button to carry out the command\n\n";
-        textAtTop += "search: find a book\n";
-        textAtTop += "addBook: add a book\n";
-        textAtTop += "setToRead: update a book to read\n";
-        textAtTop += "rate: rate a book\n";
-        textAtTop += "getBooks: retrieve and display a list of books\n";
-        textAtTop += "suggestRead: choose a random book from library\n";
-        textAtTop += "addBooks: add files to your library\n\n";
-        textAtTop += "When done, simply exit out of the window\n";
+        String textAtTop = "<html>---------------------------<br>";
+        textAtTop += "- WELCOME TO YOUR LIBRARY -<br>";
+        textAtTop += "---------------------------<br><br>";
+        textAtTop += "Enter one of the following commands in the text box,<br>";
+        textAtTop += "then press the 'Enter' button to carry out the command<br><br>";
+        textAtTop += "search: find a book<br>";
+        textAtTop += "addBook: add a book<br>";
+        textAtTop += "setToRead: update a book to read<br>";
+        textAtTop += "rate: rate a book<br>";
+        textAtTop += "getBooks: retrieve and display a list of books<br>";
+        textAtTop += "suggestRead: choose a random book from library<br>";
+        textAtTop += "addBooks: add files to your library<br><br>";
+        textAtTop += "When done, simply exit out of the window<br>";
         primaryLabel = new JLabel(textAtTop, JLabel.CENTER);
+        primaryLabel.setHorizontalAlignment(SwingConstants.CENTER);
         // Nonsense numbers for now- adjust as necessary
-        primaryLabel.setSize(300, 400);
-        this.add(primaryLabel);
+        primaryLabel.setSize(350, 450);
+        this.add(primaryLabel, BorderLayout.NORTH);
 
-        // Sets up the text field for the text that will be displayed when the user presses 
-        // the "Enter" button
-        buttonOutputText = new JLabel("", JLabel.RIGHT);
-        buttonOutputText.setSize(100, 100);
-        this.add(buttonOutputText);
+        // Sets up the label associated with the output of the button for the combo box
+        // Note that we currently have arbitrary size for this button
+        // Also need to figure out how to place well within the window
+        buttonOutputLabel = new JLabel("", SwingConstants.CENTER);
+        buttonOutputLabel.setSize(350, 100);
+        buttonOutputLabel.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        buttonOutputLabel.setForeground(Color.RED);
 
         // Adds the panel
-        panel = new JPanel();
+        panel = new JPanel(new BorderLayout());
         this.add(panel);
+        panel.add(buttonOutputLabel, BorderLayout.SOUTH);
 
         // Adds a window listener for closing the window
         this.addWindowListener(new WindowAdapter() {
@@ -77,39 +98,46 @@ public class MyLibraryGUI extends JFrame {
         Sets up the necessary text fields 
      */
     private void start() {
-        // Sets up the submit button associated with the primary text field
-        JButton primaryTextFieldButton = new JButton("Enter");
-        primaryTextFieldButton.setActionCommand("entered");
-        panel.add(primaryTextFieldButton);
+        // Sets up the combo box, with all of the possible options that the user can select from
+        String[] optionsArray = {"", "search", "addBook", "setToRead", "rate", "getBooks", "suggestRead", "addBooks"};
+        optionsComboBox = new JComboBox<String>(optionsArray);
 
+        // Adds this combo box to the panel, along with a label to its left prompting the user to select
+        // one of the options within the combo box
+        JPanel submitPanel = new JPanel();
+        JLabel comboLabel = new JLabel("Select from the possible set of actions: ");
+        submitPanel.add(comboLabel);
+        submitPanel.add(optionsComboBox);
 
-        // Sets up the main text field, which will have a width of 15, and which will be situated
-        // at the center of our panel
-        primaryTextField = new JTextField("Enter library command here", 15);
-
-        panel.add(primaryTextField);
+        // Sets up the button associated with this combo box, which will be pressed when the user wants 
+        // to submit their current selection in the combo box
+        comboBoxSubmitButton = new JButton("Submit");
+        comboBoxSubmitButton.setActionCommand("submitted");
+        comboBoxSubmitButton.addActionListener(new ButtonListener());
+        submitPanel.add(comboBoxSubmitButton);
+        panel.add(submitPanel, BorderLayout.CENTER);
     }
 
     /*
         Button listener
      */
-    private class primaryTextFieldEnterListener implements ActionListener {
+    private class ButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
-            if (command.equals("entered")) {
-                String commandType = primaryTextField.getText();
+            if (command.equals("submitted")) {
+                String commandType = (String) optionsComboBox.getSelectedItem();
                 commandType = commandType.toLowerCase();
 
-                // Checks to see whether the given command is one of our 
+                // Checks to see whether the user selected an appropriate command
                 if (! (commandType.equals("search") || commandType.equals("addbook") || commandType.equals("settoread")
                         || commandType.equals("rate") || commandType.equals("getbooks") || commandType.equals("suggestread")
                         || commandType.equals("addbooks"))) {
                     // If not, then we inform the user of this, and do nothing
-                    buttonOutputText.setText("This is not one of the supported commands. Please try again");
+                    buttonOutputLabel.setText("Please select one of the specified commands");
                 } else {
                     // If it is one of our supported commands, then we set our button output label to
                     // be blank
-                    buttonOutputText.setText("");
+                    buttonOutputLabel.setText("");
 
                     // We then call the appropriate helper method for the particular task
                     if (commandType.equals("search")) {
@@ -128,6 +156,26 @@ public class MyLibraryGUI extends JFrame {
                         addBooksHelper();
                     }
                 }
+            } else if (command.equals("addBooksSubmit")) {
+                // Gets the current text from the text field
+                String curText = addBooksTextField.getText();
+
+                // Checks to see whether this file exists and is readable
+                File givenFile = new File(curText);
+                if (! (givenFile.exists() && givenFile.canRead())) {
+                    addBooksErrorLabel.setText("Error: given file cannot be read. Please try again");
+                } else {
+                    // Otherwise, we make the error label blank, and read the books from the given
+                    // file into our library collection
+                    addBooksErrorLabel.setText("");
+                    controller.cAddBooksFromFile(curText);
+
+                    // We then close the current window, since the job is done
+                    addBooksWindow.dispose();
+
+                    // We also give the user a message that the books were added successfully
+                    buttonOutputLabel.setText("Books added successfully from file");
+                }
             }
         }
     }
@@ -136,7 +184,44 @@ public class MyLibraryGUI extends JFrame {
         Helper method for the addBooks functionality
      */
     private void addBooksHelper() {
-    
+        // Creates a new window, which will be used for the addBooks functionality
+        addBooksWindow = new JFrame("addBooks Window");
+        addBooksWindow.setSize(500, 300);
+        addBooksWindow.setVisible(true);
+
+        // Constructs a panel, which will be used to control the layout
+        JPanel addBooksPanel = new JPanel(new BorderLayout());
+        addBooksWindow.add(addBooksPanel);
+
+        // Separate panel containing just the text field and button
+        JPanel textPanel = new JPanel();
+
+        // Constructs a label for the below text field
+        JLabel addBooksTextFieldLabel = new JLabel("Enter file name here:", SwingConstants.CENTER);
+        textPanel.add(addBooksTextFieldLabel);
+
+
+        // Constructs a text field, which will be used to get input from the user
+        addBooksTextField = new JTextField("", 20);
+        textPanel.add(addBooksTextField);
+        addBooksTextField.setSize(300, 100);
+
+        // Constructs an error label for the addBooks text field entry
+        addBooksErrorLabel = new JLabel("", SwingConstants.CENTER);
+        addBooksErrorLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        addBooksErrorLabel.setForeground(Color.RED);
+        addBooksPanel.add(addBooksErrorLabel, BorderLayout.SOUTH);
+
+        // Constructs a button, which will be used to determine when to get text from the 
+        // text field
+        JButton textFieldButton = new JButton("Submit");
+        textFieldButton.setActionCommand("addBooksSubmit");
+        textFieldButton.addActionListener(new ButtonListener());
+        textPanel.add(textFieldButton);
+
+        // Adds our separate panel to the overall panel
+        addBooksPanel.add(textPanel, BorderLayout.NORTH);
+
     }
 
     /*
